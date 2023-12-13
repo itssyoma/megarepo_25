25c762b0c6090d8e001ea529687e8adafb8ab44f
itssyoma <itssyoma@gmail.com>
2023-12-13 12:46:32 +0300

    Update task and report

--- task.py
+++ task.py
@@ -9,10 +9,9 @@ if __name__ == "__main__":
     scores = tuple(map(int, input("Введите баллы 20 команд в поорядке убывания: ").split()))
     if len(scores) < 20:
         print("Количество команд меньше 20", file=sys.stderr)
-        exit(1)
-    for i, num in enumerate(scores):
-        if i == 0: continue
-        if num > scores[i-1]: 
+        exit(1)        
+    for num1, num2 in zip(scores, scores[1:]):
+        if num2 > num1:
             print("Баллы расположены не в порядке убывания", file=sys.stderr)
             exit(1)
     
@@ -21,7 +20,6 @@ if __name__ == "__main__":
         print("Такого балла нет", file=sys.stderr)
         exit(1)
     
-    position = (i for i in range(1, 21))
-    result = dict(zip(scores, position))
+    result = {score: position for position, score in enumerate(scores, start=1)}
 
     print("Команда с данным баллом заняла", result[n], "место")
